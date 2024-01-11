04e785fb4bf68bbb420bdfddcaf3ce5d23aa2316
Erick Rosa <erickpmotta@gmail.com>
2024-01-11 01:18:37 -0300

    Update the occurrences_view function to transform the data into the heatmap format

--- fogo_cruzado/views.py
+++ fogo_cruzado/views.py
@@ -15,10 +15,11 @@ def occurrences_view(request):
         for occurrence in processed_data:
             occurrence_dict = {
                 'occurrence_id': occurrence.occurrence_id,
-                'latitude': occurrence.latitude,
-                'longitude': occurrence.longitude,
+                'lat': occurrence.latitude,
+                'lng': occurrence.longitude,
                 'address': occurrence.address,
                 'date': occurrence.date,
+                'weight': 1, # TODO: Calculate weight based on 'police_action' and 'agent_presence
                 'police_action': occurrence.police_action,
                 'agent_presence': occurrence.agent_presence,
                 'context_info': occurrence.context_info,  
