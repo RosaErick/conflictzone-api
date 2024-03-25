64782875ae6c05b367a4e33d7684a755fcba060c
Erick Rosa <erickpmotta@gmail.com>
2024-03-25 15:12:27 -0300

    add static root config

--- django_report/settings.py
+++ django_report/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/root/app/PatrolAnalytics-API/staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
